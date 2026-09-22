ce3acece3ce2f4bf1095ec60a2838345373cd5c8
deivi313 <deiviboja@gmail.com>
2026-09-22 21:04:45 +0200

    testing server

--- backend/config.js
+++ backend/config.js
@@ -1,22 +1,12 @@
 module.exports = {
   port: 5000,
-<<<<<<< HEAD
   clientUrl: 'http://localhost:5173',
 
   db: {
     host: 'localhost',
-    port: 3306,
+    port: 3306, // Change to 3001 if your MySQL/database runs on 3001
     name: 'rmn_auth',
     user: 'root',
-=======
-  clientUrl: 'http://localhost:5173', 
-
-  db: {
-    host: 'localhost',
-    port: 3001,
-    name: 'rmn_auth', 
-    user: 'root', 
->>>>>>> 99a39f88c1b8db42b7c6b06c6afd1d222cb711d6
     password: 'root',
   },
 
